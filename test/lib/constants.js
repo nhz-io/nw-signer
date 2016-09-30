74dd55960df05e60b5dfd4cb7f33633c6e725aa3
Ishi Ruy <dev@nhz.io>
2016-10-01 01:49:18 +0300

    added more constants

--- test/lib/constants.js
+++ test/lib/constants.js
@@ -1,6 +1,18 @@
 import test from 'ava'
-import {ITEM_ID, ITEM_VERSION} from '../../lib/constants'
+import {
+    ITEM_ID,
+    ITEM_VERSION,
+    MANIFEST,
+    PROTECTED,
+    PROTECTED_B64,
+} from '../../lib/constants'
 
 test('ITEM_ID', t => t.is(ITEM_ID, 'abcdefghijklmnopabcdefghijklmnop'))
 
 test('ITEM_VERSION', t => t.is(ITEM_VERSION, '1.2.3'))
+
+test('MANIFEST', t => t.is(MANIFEST, 'package.json'))
+
+test('PROTECTED', t => t.is(PROTECTED, '{"alg":"RS256"}'))
+
+test('PROTECTED_B64', t => t.is(PROTECTED_B64, 'eyJhbGciOiJSUzI1NiJ9'))
